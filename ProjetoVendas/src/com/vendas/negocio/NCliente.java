7614fb834849f7771de1506f9b51c67d224d44c3
gisomar <gisomarjr1994@gmail.com>
2014-10-13 15:42:28 -0300

    Criação de DAOGenerico

--- ProjetoVendas/src/com/vendas/negocio/NCliente.java
+++ ProjetoVendas/src/com/vendas/negocio/NCliente.java
@@ -3,14 +3,18 @@ package com.vendas.negocio;
 import com.vendas.basicas.Cliente;
 import com.vendas.basicas.Endereco;
 import com.vendas.dao.DAOCliente;
+import com.vendas.dao.DAOClienteGenerico;
 
 public class NCliente {
 
 	DAOCliente daoc = new DAOCliente();
 	
+	DAOClienteGenerico daogc = new DAOClienteGenerico();
+	
 	public void cadastrar(Cliente cliente) throws Exception{
 		
-		daoc.cadastrar(cliente);
+		//daoc.cadastrar(cliente);
+		daogc.persist(cliente);
 	}
 
 	public Cliente consultar(Integer id) {
