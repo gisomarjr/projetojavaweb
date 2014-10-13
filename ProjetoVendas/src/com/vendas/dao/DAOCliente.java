95933f62eb9942730d515c091669ae81236871a0
gisomar <gisomarjr1994@gmail.com>
2014-10-13 15:47:17 -0300

    Implementação DAOCliente

--- ProjetoVendas/src/com/vendas/dao/DAOCliente.java
+++ ProjetoVendas/src/com/vendas/dao/DAOCliente.java
@@ -1,56 +1,116 @@
 package com.vendas.dao;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import com.vendas.basicas.Cliente;
-import com.vendas.basicas.Endereco;
-import com.vendas.negocio.ICliente;
 
-public class DAOCliente implements ICliente  {
 
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("mysql");
-	
-	EntityManager em = emf.createEntityManager();
 
-	EntityTransaction et = em.getTransaction();
-	
-	public void cadastrar(Cliente cliente) throws Exception {	
-	try{	
-		et.begin();
-		em.persist(cliente);
-		et.commit();
-		emf.close();
-		em.close(); 
-		
-	}catch(Exception e){
-		throw new Exception("N�o foi poss�vel Inserir os dados (Cliente)");
-	}
-}
+public class DAOCliente {
 
-	public void editar(Cliente cliente) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void excluir(Cliente cliente) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public Cliente consultarPorId(Integer id) {
-
-		return em.find(Cliente.class, id);
-	}
-
-	public ArrayList<Cliente> listar() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
+	protected EntityManager entityManager;
+	 
+	/**
+	 * Construtor para pegar o EntityManager
+	 */
+    public DAOCliente() {
+        entityManager = getEntityManager();
+    }
+    
+ 
+    /**
+     * Verificando se já existe a conexão com o banco
+     * @return
+     */
+    private EntityManager getEntityManager() {
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("mysql");
+        if (entityManager == null) {
+            entityManager = factory.createEntityManager();
+        }
+ 
+        return entityManager;
+    }
+ 
+    /**
+     * Recebendo informação do Cliente pelo ID
+     * @param id
+     * @return
+     */
+    public Cliente getById(final Integer id) {
+        return entityManager.find(Cliente.class, id);
+    }
+    
+    /**
+     * Listando os Clientes
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public List<Cliente> findAll() {
+        return entityManager.createQuery("FROM " + Cliente.class.getName())
+                .getResultList();
+    }
+ 
+    /**
+     * Adicionando um novo Cliente no banco
+     * @param Cliente
+     */
+    public void persist(Cliente Cliente) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(Cliente);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+    }
+ 
+    /**
+     * Atualizando o Cliente
+     * @param Cliente
+     */
+    public void merge(Cliente Cliente) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(Cliente);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+    }
+ 
+    /**
+     * Removendo o Cliente
+     * @param Cliente
+     */
+    public void remove(Cliente Cliente) {
+        try {
+            entityManager.getTransaction().begin();
+            Cliente = entityManager.find(Cliente.class, Cliente.getId());
+            entityManager.remove(Cliente);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+    }
+ 
+    /**
+     * Removendo pelo ID
+     * @param id
+     */
+    public void removeById(final Integer id) {
+        try {
+            Cliente Cliente = getById(id);
+            remove(Cliente);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- ProjetoVendas/src/com/vendas/negocio/NCliente.java
+++ ProjetoVendas/src/com/vendas/negocio/NCliente.java
@@ -3,23 +3,23 @@ package com.vendas.negocio;
 import com.vendas.basicas.Cliente;
 import com.vendas.basicas.Endereco;
 import com.vendas.dao.DAOCliente;
-import com.vendas.dao.DAOClienteGenerico;
+import com.vendas.dao.DAOCliente;
 
 public class NCliente {
 
 	DAOCliente daoc = new DAOCliente();
 	
-	DAOClienteGenerico daogc = new DAOClienteGenerico();
+	
 	
 	public void cadastrar(Cliente cliente) throws Exception{
 		
-		//daoc.cadastrar(cliente);
-		daogc.persist(cliente);
+		
+		daoc.persist(cliente);
 	}
 
 	public Cliente consultar(Integer id) {
 		
-		return daoc.consultarPorId(id);
+		return daoc.getById(id);
 	}
 	
 }
